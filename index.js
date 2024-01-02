d50e9abb558818d18ada46d6ab03447a5fecf826
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-01-02 17:03:51 +0600

    get route implemented and get all json data

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('autoTech').collection('services');
+
+    app.get('/services', async(req, res) =>{
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -39,7 +48,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => { 
-  res.send('Hello! Server is running!');
+  res.send('Server is running!');
 })
 
 
