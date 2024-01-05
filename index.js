2a87c4d3a150a832b166dc2f6bb74bf52d2086f6
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-01-05 21:55:23 +0600

    single data insert POST method implemented

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'); 
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require ('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,11 +28,28 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('autoTech').collection('services');
+    const bookingCollection = client.db('autoTech').collection('bookings');
 
     app.get('/services', async(req, res) =>{
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    })
+
+    app.get('/service/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const options = {
+        projection: {title: 1, top: 1, description: 1, detailDescription: 1},
+      };
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    //bookings
+    app.post('/bookings', async(req, res) =>{
+      const booking = req.body;
+      console.log(booking);
     })
 
     // Send a ping to confirm a successful connection
