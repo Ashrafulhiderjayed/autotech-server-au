b37303158d5271123d1bda10cf916235a498afc5
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-01-13 15:26:36 +0600

    delete method implemented

--- index.js
+++ index.js
@@ -41,33 +41,38 @@ async function run() {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const options = {
-        projection: {title: 1, top: 1, description: 1, detailDescription: 1},
+        projection: {title: 1, top: 1, description: 1, detailDescription: 1, img: 1},
       };
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
     });
 
-    //bookings
-    app.get('/bookings', async(req, res) => {
+    //appointments
+    app.get('/appointments', async(req, res) => {
       const result = await bookingCollection.find().toArray();
       res.send(result);
     });
 
-    app.get('/mybookings', async(req, res) =>{
+    app.get('/myappointments', async(req, res) =>{
       let query = {};
       if(req.query?.email){
         query = {email: req.query.email}
       }
       const result = await bookingCollection.find(query).toArray();
       res.send(result);
-    })
-    
-    
+    });
 
-    app.post('/bookings', async(req, res) =>{
+    app.post('/appointments', async(req, res) =>{
       const booking = req.body;
       console.log(booking);
       const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.delete('/appointments/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
       res.send(result);
     })
 
