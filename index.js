369e58239cf17e116beace5553aa6112131ec5c4
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-02-17 23:14:30 +0600

    dashboard route created

--- index.js
+++ index.js
@@ -81,6 +81,15 @@ async function run() {
     })
 
     //Technitian------------------------
+    app.get('/carts', async(req, res) =>{
+      const email = req.query.email;
+      if(!email){
+        res.send([]);
+      }
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
     app.post('/carts', async(req, res) =>{
       const item = req.body;
       console.log(item);
