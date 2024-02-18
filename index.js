f7fb065d41dd42256bb5deb6c6ac5ced98034c2a
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-02-18 12:06:47 +0600

    post route created for users

--- index.js
+++ index.js
@@ -27,10 +27,18 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db('autoTech').collection('users');
     const serviceCollection = client.db('autoTech').collection('services');
     const bookingCollection = client.db('autoTech').collection('bookings');
     const cartCollection = client.db('autoTech').collection('carts');
+    //users related apis
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user)
+      res.send(result);
+    })
 
+    // menu related apis
     app.get('/services', async(req, res) =>{
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -95,6 +103,14 @@ async function run() {
       console.log(item);
       const result= await cartCollection.insertOne(item);
       res.send(result)
+    })
+
+    app.delete('/carts/:id', async (req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+
     })
 
     // Send a ping to confirm a successful connection
